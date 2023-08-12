8d45595d772d7e6eba5f351c9169da087cdba0cd
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-08-12 13:15:36 -0500

    step 6a
    8/12/2023

---
+++ Ninja Run AS/Assets/Scripts/Pickups.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pickups : MonoBehaviour
+{
+    
+    public Text scoreText;
+
+    void Start()
+    {
+       
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+    if(other.gameObject.CompareTag("coin")){
+        
+    }
+    // Debug.Log("Trigger Enter");
+    }
+}
