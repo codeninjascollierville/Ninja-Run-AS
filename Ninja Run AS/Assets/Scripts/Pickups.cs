f081897724490afb5dffb937d6ac11bfeef4ee7f
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-08-16 17:43:41 -0500

    on prove your self
    8/16/2023

--- Ninja Run AS/Assets/Scripts/Pickups.cs
+++ Ninja Run AS/Assets/Scripts/Pickups.cs
@@ -5,18 +5,29 @@ using UnityEngine.UI;
 
 public class Pickups : MonoBehaviour
 {
-    
+    public int score;
     public Text scoreText;
+
+    public ParticleSystem Pickup;
 
     void Start()
     {
-       
+       Pickup.Stop();
     }
 
     void OnTriggerEnter(Collider other)
     {
     if(other.gameObject.CompareTag("coin")){
-        
+        score++;
+        scoreText.text = score.ToString();
+       
+       
+        Debug.Log(score);
+       
+       
+        Destroy(other.gameObject);
+    Pickup.Play();
+   
     }
     // Debug.Log("Trigger Enter");
     }
